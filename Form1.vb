959bf4ca5f65288050ea37809631be2abc0ba2f1
Holly Goodwill <CMP_HoGoodwill@ects.org>
2024-03-26 09:53:28 -0400

    upg1

--- Form1.vb
+++ Form1.vb
@@ -4,7 +4,18 @@ Imports Newtonsoft.Json
 Public Class Form1
 
 
+
     Private questionList As New List(Of Question)
+    ' Private filename As String = "C:\Users\CMP_HoGoodwill\Downloads\vbchapter5kahoot.json"
+    Private Sub writeout()
+
+        For i As Integer = 0 To questionList.Count - 1
+
+            Console.WriteLine(questionList(i))
+
+        Next
+
+    End Sub
 
     Private Sub LoadDataFileFromFile(filename As String)
 
@@ -19,14 +30,19 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         LoadDataFileFromFile("vbchapter5kahoot.json")
+
     End Sub
+
 End Class
 
 Public Class Question
+
     Public question As String
     Public time As Integer
     Public answers As List(Of String)
     Public correct As Integer
+
 End Class
 
